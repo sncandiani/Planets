5c8557adccf44f392847d448ee9914d9b640fdb1
sncandiani <sofiancandiani@gmail.com>
2020-04-10 15:51:10 -0500

    Initialized planets with first practice

---
+++ planets.py
@@ -0,0 +1,8 @@
+planet_list = ["Mercury", "Mars"]
+planet_list.append("Jupiter")
+planet_list.append("Saturn")
+
+planet_list.extend(["Mars", "Mercury"])
+
+planet_list.insert(2, "Earth")
+planet_list.insert(1, "Venus")
